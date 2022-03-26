94872ad21a099b13397d3ec7cc3e4e3687461253
mahdibohloul <mahdi_bohloul@yahoo.com>
2022-03-27 00:31:10 +0430

    Connect to data channel

--- client.cpp
+++ client.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 
 #define BUFFER_SIZE 1024
+#define BUFFER_DATA_SIZE 1048576
 #define COMMAND_CHANNEL_PORT 8000
 #define DATA_CHANNEL_PORT 8001
 #define RUNNING 1
@@ -14,12 +15,19 @@
 
 int main() {
     //declaring important data
-    int client_fd;
+    int client_command_fd;
+    int client_data_fd;
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
+    char buffer_data[BUFFER_DATA_SIZE];
 
     //Creating a server socket
-    if (!(client_fd = socket(AF_INET, SOCK_STREAM, 0))) {
+    if (!(client_command_fd = socket(AF_INET, SOCK_STREAM, 0))) {
+        printf("Failed to create socket!\nExiting..");
+        exit(1);
+    }
+
+    if (!(client_data_fd = socket(AF_INET, SOCK_STREAM, 0))) {
         printf("Failed to create socket!\nExiting..");
         exit(1);
     }
@@ -29,11 +37,22 @@ int main() {
     server_addr.sin_port = htons(COMMAND_CHANNEL_PORT);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if ((connect(client_fd, (struct sockaddr *) &server_addr, sizeof(server_addr))) < 0) {
+    if ((connect(client_command_fd, (struct sockaddr *) &server_addr, sizeof(server_addr))) < 0) {
         printf("Failed to connect to the server!\nExiting\n");
         exit(1);
     }
-    std::cout << "client fd: " << client_fd << std::endl;
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(DATA_CHANNEL_PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if ((connect(client_data_fd, (struct sockaddr *) &server_addr, sizeof(server_addr))) < 0) {
+        printf("Failed to connect to the server!\nExiting\n");
+        exit(1);
+    }
+
+    std::cout << "client command fd: " << client_command_fd << std::endl;
+    std::cout << "client data fd: " << client_data_fd << std::endl;
     while (RUNNING) {
         bzero(buffer, BUFFER_SIZE);
         fgets(buffer, BUFFER_SIZE, stdin);
@@ -44,9 +63,16 @@ int main() {
         char *cmd = strtok(cmd_line, " ");
 
 
-        send(client_fd, buffer, BUFFER_SIZE, 0);
+        send(client_command_fd, buffer, BUFFER_SIZE, 0);
         memset(buffer, 0, BUFFER_SIZE);
-        read(client_fd, buffer, BUFFER_SIZE);
+        read(client_command_fd, buffer, BUFFER_SIZE);
         std::cout << buffer << std::endl;
+        std::string command_resp(buffer);
+        if (command_resp.substr(0, 3) == "226")
+        {
+            memset(buffer_data, 0, sizeof(buffer_data));
+            read(client_data_fd, buffer_data, BUFFER_DATA_SIZE);
+            std::cout << buffer_data << std::endl;
+        }
     }
 }
